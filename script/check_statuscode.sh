1eb7db053565b899da7909df903554630ee26018
huanpham15101999 <70229853+huanpham15101999@users.noreply.github.com>
2022-07-20 10:42:47 +0700

    Update check_statuscode.sh

--- script/check_statuscode.sh
+++ script/check_statuscode.sh
@@ -5,7 +5,7 @@ echo -e "\e[1;32mstatus http"
 echo -e "\e[1;31m------------------------------------------------------------"
 
 while read LINE; do
-  curl -o /dev/null --silent --head --write-out "%{http_code} http://$LINE\n" "$LINE"
+  curl -o /dev/null --silent --head --write-out "%{http_code} http://$LINE\n" "http://$LINE"
 done < statuscode.txt
 
 echo -e "\e[1;31m------------------------------------------------------------"
@@ -13,7 +13,7 @@ echo -e "\e[1;32mstatus https"
 echo -e "\e[1;31m------------------------------------------------------------"
 
 while read LINE; do
-  curl -o /dev/null --silent --head --write-out "%{http_code} https://$LINE\n" "$LINE"
+  curl -o /dev/null --silent --head --write-out "%{http_code} https://$LINE\n" "https://$LINE"
 done < statuscode.txt
 
 # statuscode.txt la file list domain

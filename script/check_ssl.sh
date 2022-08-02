906b6b3dc3eb128a505dbc5bd8cbe2b96a5cee0c
huanpham15101999 <70229853+huanpham15101999@users.noreply.github.com>
2022-08-02 16:50:55 +0700

    Update check_ssl.sh

--- script/check_ssl.sh
+++ script/check_ssl.sh
@@ -4,3 +4,4 @@ read a
 echo "Nhap VIP: "
 read b
 echo | openssl s_client -servername $a -connect $b:443 2>/dev/null | openssl x509 -noout -dates
+# Check pem file: openssl x509 -enddate -noout -in file.pem

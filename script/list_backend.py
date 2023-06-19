c97d95ef6f8b1df52ed7a3baf851ba159dd15d2e
huanpham15101999 <70229853+huanpham15101999@users.noreply.github.com>
2023-06-19 16:47:25 +0700

    Update list_backend.py

--- script/list_backend.py
+++ script/list_backend.py
@@ -4,11 +4,17 @@ def get_backend_list(config_file):
         lines = file.readlines()
         for line in lines:
             line = line.strip()
+            while "  " in line:
+                line = line.replace("  ", " ")
             if line.startswith('backend'):
                 backend_name = line.split(' ')[1]
                 backends.append(backend_name)
     return backends
-
-config_file = 'haproxy.cfg'  # Thay thế bằng đường dẫn tệp cấu hình Haproxy của bạn
+    
+config_file = 'all.txt'
 backends = get_backend_list(config_file)
-print(backends)
+
+for item in backends:
+    print(item)
+
+print(len(backends))

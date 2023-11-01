5d12342f9c1723146c62075cfcbd870fd2460633
huanpham15101999 <70229853+huanpham15101999@users.noreply.github.com>
2023-11-01 14:19:52 +0700

    Update monitor-website.py

--- script/monitor-website.py
+++ script/monitor-website.py
@@ -7,21 +7,29 @@ EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 response = requests.get('http://10.5.83.36:8080')
 
+
+def send_notification(email_msg):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        message = f"Subject:  SITE DOWN\n{email_msg}"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+
+
 try:
     if response.status_code == 200:
         print("Application is running successfully")
     else:
         print("Application Down. Fix it!")
+        msg = f"Subject: Application returned {response.status_code}"
+        send_notification(msg)
 
-        # send email to me
-        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-            smtp.starttls()
-            smtp.ehlo()
-            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            msg = "Subject:  SITE DOWN\nFix the issue! Restart the application."
-            smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
 except Exception as ex:
     print(f"Connection error happened: {ex}")
+    msg = "Application is not accessible at all"
+    send_notification(msg)
+    
 
 
 

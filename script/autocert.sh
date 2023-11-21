9b059ef7d7ba9c83ef5cba258fdb7cd8bf955d7a
huanpham15101999 <70229853+huanpham15101999@users.noreply.github.com>
2023-11-21 10:01:53 +0700

    Update autocert.sh

--- script/autocert.sh
+++ script/autocert.sh
@@ -2,6 +2,7 @@
 for cert in `ls | grep ".key" | awk '{split($0,a,".key"); print a[1]}'`
 do
   cat $cert.key $cert.cer > $cert.pem
+  sed -i '/^subject/d;/^issuer/d;/^$/d' $cert.pem
   echo -ne "\e[0;36m$cert.pem "  
   checkdate=$(openssl x509 -noout -enddate -in $cert.pem)
   echo -ne "\e[0;31m$checkdate\n"
